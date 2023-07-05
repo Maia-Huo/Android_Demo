02cdbb62244b82c4013969a4c947920ea673b461
Maia-Huo <3141193493@qq.com>
2023-07-05 18:40:39 +0800

    设置SimpleAdapter,显示标题及作者

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,24 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String NEWS_TITLE = "news_title";
+    private static final String NEWS_AUTHOR = "news_author";
+
+    /*使⽤ List<Map<String, String> > dataList; 替换掉之前定义的
+    titles、authors 数组。将数据源的构造操作放⼊initData()⽅法中*/
+    private List<Map<String, String>> dataList =  new ArrayList<>();
     private String[] titles;
     private String[] authors;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        titles = getResources().getStringArray(R.array.titles);//获取字符串数组
-        authors = getResources().getStringArray(R.array.authors);
-        //船舰名为adapter的ArrayAdapter对象，用于与数组源（即titles数组）与列表视图进行绑定
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, titles);
+        initData();
 
-        ListView lvNewsList = findViewById(R.id.lv_news_list);
-        lvNewsList.setAdapter(adapter);
+        SimpleAdapter simpleAdapter = new SimpleAdapter(
+                this, dataList, android.R.layout.simple_list_item_2,
+                new String[] { NEWS_TITLE, NEWS_AUTHOR },
+                new int[] { android.R.id.text1, android.R.id.text2});
+
+        ListView listView = findViewById(R.id.lv_news_list);
+        listView.setAdapter(simpleAdapter);
+    }
+
+    private void initData() {
+        int length;
+        titles = getResources().getStringArray(R.array.titles);
+        authors = getResources().getStringArray(R.array.authors);
+
+        if(titles.length > authors.length) {
+            length = authors.length;
+        } else {
+            length = titles.length;
+        }
+
+        for(int i = 0; i < length; i++) {
+            Map map = new HashMap();
+            map.put(NEWS_TITLE, titles[i]);
+            map.put(NEWS_AUTHOR, authors[i]);
+            dataList.add(map);
+        }
     }
 }
