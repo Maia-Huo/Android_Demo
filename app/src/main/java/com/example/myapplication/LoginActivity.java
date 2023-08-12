c6f3a76842b000ae48ef47c6ac960133df8dfe83
Maia-Huo <3141193493@qq.com>
2023-08-12 12:45:56 +0800

    完成注册逻辑

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,17 +2,22 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -31,10 +36,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         etAccount = findViewById(R.id.et_account);
         cbRememberPwd = findViewById(R.id.cb_remember_pwd);
         Button btLogin = findViewById(R.id.bt_login);
+        Button btRegister = findViewById(R.id.bt_register);
 
         //点击之后除登录外，还要检查如果勾选保存密码需要保存
         btLogin.setOnClickListener(this);
-
+        btRegister.setOnClickListener(this);
 
         //密码明文密文以及图标切换
         ivPwdSwitch.setOnClickListener(new View.OnClickListener() {
@@ -92,38 +98,69 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     //设置SharedPreferences
     //登录按钮点击事件，如果勾选保存密码，将信息存入SharedPreferences
     public void onClick(View view) {
-        String spFileName = getResources()
-                .getString(R.string.shared_preferences_file_name);
-        String accountKey = getResources()
-                .getString(R.string.login_account_name);
-        String passwordKey =  getResources()
-                .getString(R.string.login_password);
-        String rememberPasswordKey = getResources()
-                .getString(R.string.login_remember_password);
+        if (view.getId() == R.id.bt_login) {
+            Toast.makeText(getApplicationContext(),"登录成功",Toast.LENGTH_SHORT).show();
+            String spFileName = getResources()
+                    .getString(R.string.shared_preferences_file_name);
+            String accountKey = getResources()
+                    .getString(R.string.login_account_name);
+            String passwordKey =  getResources()
+                    .getString(R.string.login_password);
+            String rememberPasswordKey = getResources()
+                    .getString(R.string.login_remember_password);
 
-        //创建一个SharedPreferences对象
-        //Context.MODE_PRIVATE是一个访问模式的常量，用于指定SharedPreferences文件的访问权限
-        //当前表示只有当前应用程序能访问SharedPreferences，其他程序不能访问修改
-        SharedPreferences spFile = getSharedPreferences(
-                spFileName,
-                Context.MODE_PRIVATE);
-        //获取编辑对象
-        SharedPreferences.Editor editor = spFile.edit();
+            //创建一个SharedPreferences对象
+            //Context.MODE_PRIVATE是一个访问模式的常量，用于指定SharedPreferences文件的访问权限
+            //当前表示只有当前应用程序能访问SharedPreferences，其他程序不能访问修改
+            SharedPreferences spFile = getSharedPreferences(
+                    spFileName,
+                    Context.MODE_PRIVATE);
+            //获取编辑对象
+            SharedPreferences.Editor editor = spFile.edit();
 
-        if(cbRememberPwd.isChecked()){
-            String password = etPwd.getText().toString();
+            if(cbRememberPwd.isChecked()){
+                String password = etPwd.getText().toString();
+                String account = etAccount.getText().toString();
+
+                editor.putString(accountKey,account);
+                editor.putString(passwordKey,password);
+                editor.putBoolean(rememberPasswordKey,true);
+                //只有调用apply()或commit()方法之后，对SharedPreferences的修改才会生效
+                editor.apply();
+            } else {
+                editor.remove(accountKey);
+                editor.remove(passwordKey);
+                editor.remove(rememberPasswordKey);
+                editor.apply();
+            }
+        }
+        if (view.getId() == R.id.bt_register) {
             String account = etAccount.getText().toString();
+            String password = etPwd.getText().toString();
 
-            editor.putString(accountKey,account);
-            editor.putString(passwordKey,password);
-            editor.putBoolean(rememberPasswordKey,true);
-            //只有调用apply()或commit()方法之后，对SharedPreferences的修改才会生效
-            editor.apply();
-        } else {
-            editor.remove(accountKey);
-            editor.remove(passwordKey);
-            editor.remove(rememberPasswordKey);
-            editor.apply();
+            //将用户信息添加到数据库中
+            DatabaseHelper dbHelper = new DatabaseHelper(this);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+            ContentValues values = new ContentValues();
+            values.put(dbHelper.getColumnUsername(),account);
+            values.put(dbHelper.getColumnPassword(),password);
+
+            long newRowId = db.insert(dbHelper.getTableName(),null,values);
+
+
+            if (newRowId != -1){
+                //表示成功将信息添加到数据库中
+                //如果插入成功，数据库会返回插入的行的ID（或者称为记录的ID），如果插入失败，通常会返回-1
+                Toast.makeText(getApplicationContext(),"Registration successful. Please log in.",Toast.LENGTH_SHORT).show();
+                //清除输入框
+                etAccount.setText("");
+                etPwd.setText("");
+            }else {
+                //注册失败
+            }
+            db.close();
         }
     }
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,76 @@
+//package com.example.myapplication;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.res.TypedArray;
+//import android.os.Bundle;
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//import android.widget.SimpleAdapter;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    public static final String NEWS_ID = "news_id";
+//    private List<News> newsList = new ArrayList<>();
+//    private String[] titles;
+//    private String[] authors;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        initData();
+//
+//        NewsAdapter newsAdapter = new NewsAdapter(MainActivity.this,
+//                R.layout.list_item, newsList);
+//
+//        ListView lvNewsList = findViewById(R.id.lv_news_list);
+//
+//        lvNewsList.setAdapter(newsAdapter);
+//    }
+//
+//    private void initData() {
+//        int length;
+//
+//        titles = getResources().getStringArray(R.array.titles);
+//        authors = getResources().getStringArray(R.array.authors);
+//        TypedArray images = getResources().obtainTypedArray(R.array.images);
+//
+//        if (titles.length > authors.length) {
+//            length = authors.length;
+//        } else {
+//            length = titles.length;
+//        }
+//
+//        for (int i = 0; i < length; i++) {
+//            News news = new News();
+//            news.setTitle(titles[i]);
+//            news.setAuthor(authors[i]);
+//            news.setImageId(images.getResourceId(i, 0));
+//
+//            newsList.add(news);
+//        }
+//    }
+//}
+
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.res.TypedArray;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -19,42 +86,63 @@ public class MainActivity extends AppCompatActivity {
     private List<News> newsList = new ArrayList<>();
     private String[] titles;
     private String[] authors;
+    private MySQLiteOpenHelper myDbHelper;
+    private SQLiteDatabase db;
+    private News news;
+    private ListView lvNewsList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initData();
+        ListView lvNewsList = findViewById(R.id.lvNewsList);
 
-        NewsAdapter newsAdapter = new NewsAdapter(MainActivity.this,
-                R.layout.list_item, newsList);
+        myDbHelper = new MySQLiteOpenHelper(MainActivity.this);
+        db = myDbHelper.getReadableDatabase();
+        Cursor cursor = db.query(
+                NewsContract.NewsEntry.TABLE_NAME,
+                null, null, null, null, null, null);
 
-        ListView lvNewsList = findViewById(R.id.lv_news_list);
+        List<News> newsList = new ArrayList<>();
 
+        int titleIndex = cursor.getColumnIndex(
+                NewsContract.NewsEntry.COLUMN_NAME_TITLE);
+        int authorIndex = cursor.getColumnIndex(
+                NewsContract.NewsEntry.COLUMN_NAME_AUTHOR);
+        int imageIndex = cursor.getColumnIndex(
+                NewsContract.NewsEntry.COLUMN_NAME_IMAGE);
+
+        while (cursor.moveToNext()) {
+            News news = new News();
+
+            String title = cursor.getString(titleIndex);
+            String author = cursor.getString(authorIndex);
+            //String image = cursor.getString(imageIndex);
+            String imageFileName = cursor.getString(imageIndex);
+
+            Log.d("ImageTest", "Image File Name: " + imageFileName);
+            int resourceId = getResources().getIdentifier(imageFileName, "drawable", getPackageName());
+            Log.d("ImageTest", "Image Resource ID: " + resourceId);
+
+
+            // 将资源ID解码成Bitmap对象
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resourceId);
+            //Bitmap bitmap = BitmapFactory.decodeStream(
+             //       getClass().getResourceAsStream("/" + image));
+
+            news.setTitle(title);
+            news.setAuthor(author);
+            news.setImageId(bitmap);
+            newsList.add(news);
+        }
+
+        NewsAdapter newsAdapter = new NewsAdapter(
+                MainActivity.this,
+                R.layout.list_item,
+                newsList);
         lvNewsList.setAdapter(newsAdapter);
     }
 
-    private void initData() {
-        int length;
-
-        titles = getResources().getStringArray(R.array.titles);
-        authors = getResources().getStringArray(R.array.authors);
-        TypedArray images = getResources().obtainTypedArray(R.array.images);
-
-        if (titles.length > authors.length) {
-            length = authors.length;
-        } else {
-            length = titles.length;
-        }
-
-        for (int i = 0; i < length; i++) {
-            News news = new News();
-            news.setTitle(titles[i]);
-            news.setAuthor(authors[i]);
-            news.setImageId(images.getResourceId(i, 0));
-
-            newsList.add(news);
-        }
-    }
 }
